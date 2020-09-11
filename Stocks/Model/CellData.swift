0a3f21fdb0351564bbacca1bf4aede7697e523c8
Alexey <lexainb3@gmail.com>
2020-09-12 02:12:40 +0300

    Some changes

---
+++ Stocks/Model/CellData.swift
@@ -0,0 +1,15 @@
+//
+//  StocksCellDataStructure.swift
+//  Stocks
+//
+//  Created by Алексей on 11.09.2020.
+
+
+import Foundation
+
+struct CellData  {
+    var name : String? = ""
+    var symbol : String? = ""
+    var price : Double? = 0.0
+    var priceChange : Double? = 0.0
+}
